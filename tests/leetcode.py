869bf5be6f8d8b67b639ead0ca20a29706546575
dmitrybugakov <dmitry.bugakov@kohls.com>
2020-01-10 16:26:01 +0300

    tets

--- tests/leetcode.py
+++ tests/leetcode.py
@@ -13,6 +13,10 @@ class BasicTestSuite(unittest.TestCase):
     def test_is_palindrome() -> object:
         assert solution.is_palindrome(101) == True, "Should be True"
 
+    @staticmethod
+    def test_median_of_two_sorted_arrays() -> object:
+        assert solution.median_of_two_sorted_arrays([1, 2], [3]) == 2, "Should be 2"
+
 
 if __name__ == '__main__':
     unittest.main()
