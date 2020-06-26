52d31bac9d321b9490f0f100bb607513b10a28ef
dmitrybugakov <dbugakov@griddynamics.com>
2020-06-26 23:04:40 +0300

    minor

--- jff/algorithms/stanford.py
+++ jff/algorithms/stanford.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def multiplication(x, y, n) -> int:
     if n == 1:
         return int(x) * int(y)
@@ -34,3 +37,38 @@ def karatsuba(x, y, n) -> int:
         adbc = pq - ac - bd
 
         return 10 ** n * ac + 10 ** k * adbc + bd
+
+
+def merge_sort(array: List[int]) -> List[int]:
+    def merge(left: List[int], right: List[int]) -> List[int]:
+        length = len(left) + len(right)
+        result: List[int] = list()
+        i: int = 0
+        j: int = 0
+        k: int = 0
+        while k < length:
+            if (i < len(left)) and (j < len(right)):
+                if left[i] <= right[j]:
+                    result.append(left[i])
+                    i += 1
+                    k += 1
+                else:
+                    result.append(right[j])
+                    j += 1
+                    k += 1
+            elif i < len(left):
+                return result + left[i:len(left)]
+            elif j < len(right):
+                return result + right[j:len(right)]
+
+        return result
+
+    def sort(array: List[int]) -> List[int]:
+        if len(array) == 1:
+            return array
+        else:
+            length = len(array)
+            middle = len(array) >> 1
+            return merge(sort(array[0:middle]), sort(array[middle:length]))
+
+    return sort(array)
--- jff/main.py
+++ jff/main.py
@@ -1,9 +1,12 @@
-from jff.algorithms.stanford import multiplication, karatsuba
+from typing import List
+
+from jff.algorithms.stanford import multiplication, karatsuba, merge_sort
 
 
 def main() -> None:
-    print(multiplication(x="1234", y="2341", n=4))
-    print(karatsuba(x="1234", y="2341", n=4))
+    # print(multiplication(x="1234", y="2341", n=4))
+    # print(karatsuba(x="1234", y="2341", n=4))
+    print(merge_sort([1, 2, 3, 4, 6, 5]))
 
 
 if __name__ == "__main__":
